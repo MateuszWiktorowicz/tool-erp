08379f71b0f778a06dd2b4b722ae271daef9bb7f
MateuszWiktorowicz <83886002+MateuszWiktorowicz@users.noreply.github.com>
2024-09-05 09:36:05 +0200

    add new user to json server, check email is used already

--- src/Components/Users/Register.jsx
+++ src/Components/Users/Register.jsx
@@ -10,15 +10,54 @@ function Register() {
     const { 
         register, 
         handleSubmit,
-        formState: {errors}
+        formState: {errors},
+        setError
     } = useForm({ 
             mode: "all",
             resolver: zodResolver(schema) 
         });
 
-    const onSubmit = (data) => {
-        console.log(data);  
-    };
+    const onSubmit = async (data) => {
+        try {
+            const usersResponse = await fetch('http://localhost:3333/users');
+            const users = await usersResponse.json();
+            const emailExists = users.some(user => user.email === data.email);
+
+            if (emailExists) {
+                
+                setError("email", {
+                    type: "manual",
+                    message: "Email is already used"
+                });
+                return; 
+            }
+        const newUser = {
+            email: data.email,
+            password: data.password
+        };
+
+        
+            
+            const response = await fetch('http://localhost:3333/users', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify(newUser),
+            });
+            if (response.ok) {
+                const savedUser = await response.json();
+                console.log('User registered successfully:', savedUser);
+            } else {
+                console.error('Failed to register user:', response.statusText);
+            }
+        } catch (error) {
+            console.error('Error while saving user:', error);
+        }
+    }
+  
+        
+        
     return(
         <form onSubmit={handleSubmit(onSubmit)}>
             <h2>Register form</h2>
