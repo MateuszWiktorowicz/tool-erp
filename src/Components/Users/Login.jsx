0a2e8cbfa1cd29320fa514ba23ada036a0ce2398
MateuszWiktorowicz <83886002+MateuszWiktorowicz@users.noreply.github.com>
2024-09-05 08:21:48 +0200

    add input validation to login form

--- src/Components/Users/Login.jsx
+++ src/Components/Users/Login.jsx
@@ -1,16 +1,34 @@
 import { useForm } from "react-hook-form";
+import { zodResolver } from '@hookform/resolvers/zod';
+
+import { schema } from './schema';
 
 
 function Login() {
-    const { register, handleSubmit } = useForm();
+    const { 
+        register, 
+        handleSubmit,
+        formState: {errors}
+    } = useForm({ 
+            mode: "all",
+            resolver: zodResolver(schema) 
+        });
 
     const onSubmit = (data) => {
         console.log(data);  
     };
     return(
         <form onSubmit={handleSubmit(onSubmit)}>
-            <input {...register("email")} type="text" placeholder="Email" />
-            <input {...register("password")} type="password" placeholder="Password" />
+            <input {...register("email")} 
+            type="text" 
+            placeholder="Email" 
+            />
+            {errors.email && <div style={{ color: 'red' }}>{errors.email.message}</div>}
+            <input {...register("password")} 
+            type="password" 
+            placeholder="Password" 
+            />
+            {errors.password && (<div style={{ color: 'red' }}>{errors.password.message}</div>)}
             <button type="submit">Submit</button>
         </form>
     )
