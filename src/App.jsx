3b7bb8280a586e35f8afb2993a60fb690ea0fef6
MateuszWiktorowicz <83886002+MateuszWiktorowicz@users.noreply.github.com>
2024-09-06 09:27:07 +0200

    add token

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,32 @@
-import { useState } from 'react'
+import { useState } from 'react';
 import './App.css'
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Header from './Components/Header'
 import Footer from './Components/Footer'
 import ProductCreator from './Components/ProductCreator/ProductCreator'
 import Login from './Components/Users/Login'
 import Register from './Components/Users/Register'
 import '@fortawesome/fontawesome-free/css/all.min.css';
+import  useToken  from './useToken';
+
+
 
 function App() {
-
+  const [token, setToken] = useToken();
 
   return (
     <>
-      <Header />
-      <ProductCreator />
-      <Login />
-      <Register />
+      <Header token={token}/>
+      <BrowserRouter>
+      <Routes>
+      {!token ? (
+            <Route path="*" element={<Login setToken={setToken} />} />
+          ) : (
+              <Route path="/" element={<ProductCreator />} />
+             
+          )}
+      </Routes>
+      </BrowserRouter>
       <Footer />
     </>
   )
